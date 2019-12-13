d7f4a493ae697f76defbd70745276065171a1200
Boothbayer <Boothbayer@github.com>
2019-12-13 17:31:01 -0500

    UPDATES....

--- robotAuto.py
+++ robotAuto.py
@@ -6,9 +6,9 @@ from __future__ import division
 from time import sleep
 import os
 import Adafruit_PCA9685
-from pyfiglet import Figlet
-ascii = Figlet(font='starwars', width=900)
-print(ascii.renderText('Autonomo'))
+#from pyfiglet import Figlet
+#ascii = Figlet(font='starwars', width=900)
+#print(ascii.renderText('Autonomo'))
 print("AUTONOMOUS MODE LOADING...")
 sleep(1)
 # Initialise the PCA9685 using the default address (0x40).
@@ -18,10 +18,10 @@ pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
 
 #Connections should be marked here, number next to variable corresponds to socket plugged into. REMEMBER: Green is Ground(G)
-frontLeft = 4
-backleft = 8
-frontRight = 0
-backRight = 15
+frontLeft =9
+backLeft = 8
+frontRight = 10
+backRight = 11
 
 #The throttle function allows a value from -1 to 1 to be input and translated into the required pwm value
 def setThrottle(x):
@@ -30,7 +30,7 @@ def setThrottle(x):
 	elif(x < -1):
 		return -1
 	else:
-		Speed = (234.5 * x + 391.5) #max of 626hz and min of 157hz
+		Speed = (222.5 * x + 377.5) #max of 626hz and min of 157hz
 		#Formulas for getting above function: [(max-min)/2]x[(max)-((max-min)/2)]
 		#626-157=469/2=234.5 | 626-234.5=391.5
 		return Speed
@@ -44,17 +44,17 @@ def setMove(x):
 	print("STATUS: Moving at",x)
 	rightThrottle = setThrottle(x)
 	leftThrottle = setThrottle(x * -1)
-	pwm.set_pwm(frontRight, 0, rightThrottle)
-	pwm.set_pwm(backRight, 0, rightThrottle)
-	pwm.set_pwm(frontLeft, 0, leftThrottle)
-	pwm.set_pwm(backLeft, 0, leftThrottle)
+	pwm.set_pwm(frontRight, 0, int(rightThrottle))
+	pwm.set_pwm(backRight, 0, int(rightThrottle))
+	pwm.set_pwm(frontLeft, 0, int(leftThrottle))
+	pwm.set_pwm(backLeft, 0, int(leftThrottle))
 def setTurn(x):
-	print("STATUS: Turning at {}",x)
-	allThrottle = setThrottle(x)
-	pwm.set_pwm(frontRight, 0, allThrottle)
-	pwm.set_pwm(backRight, 0, allThrottle)
-	pwm.set_pwm(frontLeft, 0, allThrottle)
-	pwm.set_pwm(backLeft, 0, allThrottle)
+	print("STATUS: Turning at {}".format(x))
+	allThrottle = setThrottle(x * -1)
+	pwm.set_pwm(frontRight, 0,int(allThrottle))
+	pwm.set_pwm(backRight, 0, int(allThrottle))
+	pwm.set_pwm(frontLeft, 0, int(allThrottle))
+	pwm.set_pwm(backLeft, 0, int(allThrottle))
 #Another Abstraction for previous two functions
 def setAction(action, speed, time): #Action = 1; Move #Action = 0;Turn
 	if(action): #1 or 0
@@ -70,14 +70,16 @@ def setAction(action, speed, time): #Action = 1; Move #Action = 0;Turn
 #------------------------------------------------------------------------------------------------------#
 #---------------------------------------Code-Begins-Here-----------------------------------------------#
 #------------------------------------------------------------------------------------------------------#
-ascii = pyfiglet.figlet_format("Autonomo")
-print(ascii)
+#ascii = pyfiglet.figlet_format("Autonomo")
+#print(ascii)
 print("-----AUTONOMO-----")
 #setAction(action,speed,time)
-print("Forward for 0.5 seconds at 50% Throttle")
-setAction(1, 0.5, 0.5)
-print("Turn for 1.5 seconds at 50% Throttle")
-setAction(0, 0.5, 1.5)
+#print("Forward for 0.5 seconds at 50% Throttle")
+#setAction(1, 0.25, 0.5)
+#print("Turn for 1.5 seconds at 50% Throttle")
+setAction(0, 0.25, 0.5)
+sleep(1)
+setAction(1, 1, 1)
 sleep(0.3)
 setMove(0)
 quit()
--- robotCode.py
+++ robotCode.py
@@ -41,20 +41,20 @@ while True:
 		rightstick = gamepad.get_axis(3)
 		Abutton = gamepad.get_button(0)
 		Bbutton = gamepad.get_button(1)
-		Xbutton = gamepad.get_button(3)
-		Ybutton = gamepad.get_button(4)
-		Lshoulder = gamepad.get_button(6)
-		Rshoulder = gamepad.get_button(7)
-		Ltrigger = gamepad.get_button(8)
-		Rtrigger = gamepad.get_button(9)
-		Backbutton = gamepad.get_button(10)
-		Startbutton = gamepad.get_button(11)
-		LeftstickButton = gamepad.get_button(13)
-		RightstickButton = gamepad.get_button(14)
-		UpDpad = gamepad.get_button(12)
-		DownDpad = gamepad.get_button(13)
-		LeftDpad = gamepad.get_button(14)
-		RightDpad = gamepad.get_button(15)
+		Xbutton = gamepad.get_button(2)
+		Ybutton = gamepad.get_button(3)
+#		Lshoulder = gamepad.get_button(6)
+#		Rshoulder = gamepad.get_button(7)
+#		Ltrigger = gamepad.get_button(8)
+#		Rtrigger = gamepad.get_button(9)
+#		Backbutton = gamepad.get_button(10)
+#		Startbutton = gamepad.get_button(11)
+#		LeftstickButton = gamepad.get_button(13)
+#		RightstickButton = gamepad.get_button(14)
+#		UpDpad = gamepad.get_button(12)
+#		DownDpad = gamepad.get_button(13)
+#		LeftDpad = gamepad.get_button(14)
+#		RightDpad = gamepad.get_button(15)
         else:
 		leftstick = 0
 		rightstick = 0
@@ -62,10 +62,10 @@ while True:
 		Rservo = 0
 
 	#set pins here
-	frontLeft = 4
-	backleft = 8
-	frontRight = 0
-	backRight = 15
+	frontLeft = 3
+	backLeft = 2
+	frontRight = 6
+	backRight = 11
 
 	#Prevents thejoystick from "floating" and makes sure values do not exceed [-1,1]
         def JoyStick(y):
@@ -85,57 +85,49 @@ while True:
 		elif(x < -1):
 			return -1
 		else:
-			Speed = (234.5 * x + 391.5)
+			Speed = (222.5 * x + 377.5)
 			return Speed
 
 	#Function for handling movement
 	def setMove(x):
-		rightThrottle = setThrottle(x)
-		leftThrottle = setThrottle(x * -1)
-		pwm.set_pwm(frontRight, 0, rightThrottle)
-		pwm.set_pwm(backRight, 0, rightThrottle)
-		pwm.set_pwm(frontLeft, 0, leftThrottle)
-		pwm.set_pwm(backLeft, 0, leftThrottle)
+		rightThrottle = setThrottle(x * -1)
+		leftThrottle = setThrottle(x) #MIN=155 #MAX=600 #setThrottle(x * -1)
+		pwm.set_pwm(frontRight, 0, int(rightThrottle))
+		pwm.set_pwm(backRight, 0, int(rightThrottle))
+		pwm.set_pwm(frontLeft, 0, int(leftThrottle))
+		pwm.set_pwm(backLeft, 0, int(leftThrottle))
 
+	def setStrafe(x):
+		rightThrottle = setThrottle(x * -1)
+		leftThrottle = setThrottle(x) #MIN=155 #MAX=600 #setThrottle(x * -1)
+		pwm.set_pwm(frontRight, 0, int(rightThrottle))
+		pwm.set_pwm(backRight, 0, int(leftThrottle))
+		pwm.set_pwm(frontLeft, 0, int(rightThrottle))
+		pwm.set_pwm(backLeft, 0, int(leftThrottle))
 	#Function for handling turning
 	def setTurn(x):
-		allThrottle = setThrottle(x)
-		pwm.set_pwm(frontRight, 0, allThrottle)
-		pwm.set_pwm(backRight, 0, allThrottle)
-		pwm.set_pwm(frontLeft, 0, allThrottle)
-		pwm.set_pwm(backLeft, 0, allThrottle)
+		allThrottle = setThrottle(x * -1)
+		pwm.set_pwm(frontRight, 0, int(allThrottle))
+		pwm.set_pwm(backRight, 0, int(allThrottle))
+		pwm.set_pwm(frontLeft, 0, int(allThrottle))
+		pwm.set_pwm(backLeft, 0, int(allThrottle))
+
+	#Function for handling arm
+	def setArm(x):
+		throttle = setThrottle(x)
+		pwm.set_pwm(7, 0, int(throttle))
 
 	#CONTROL
+	if(Abutton): #DOWN
+		setArm(0.2)
+	elif(Bbutton): #UP
+		setArm(-0.3)
+	else:
+		setArm(-0.05)
+
         if((JoyStick(leftstickX) > 0.2) or (JoyStick(leftstickX) < -0.2)):
 		setTurn(JoyStick(leftstickX))
-        else:
+        elif((JoyStick(rightstick) > 0.2) or (JoyStick(rightstick) < -0.2)):
+		setStrafe(JoyStick(rightstick))
+	else:
 		setMove(JoyStick(leftstick))
-
-#        def Throttle(x):
- #           if(x > 1):
-  #              return 1
-   #         elif(x < -1):
-    #            return -1
-     #       else:
-      #          Speed = (234.5 * x + 391.5)
-       #         return Speed
-
-#        #Green is GROUND and Yellow is SIGNAl
-#        LmotorR = (225 * Throttle(JoyStick(leftstick)) + 376)
-#        LmotorL = (-225 * Throttle(JoyStick(leftstick) + 376))
-#        if((JoyStick(leftstickX) > 0.2) or (JoyStick(leftstickX) < -0.2)):
-#         #TURNING MOVEMENT
-#	    LmotorR = Throttle(JoyStick(leftstickX) * 1) #(225 * JoyStick(leftstickX) + 376)
-#            LmotorL = Throttle(JoyStick(leftstickX) * 1) #(225 * JoyStick(leftstickX) + 376)
-#            pwm.set_pwm(0, 0, int(LmotorR)) #front right
-#            pwm.set_pwm(15, 0, int(LmotorR)) #back right
-#            pwm.set_pwm(4, 0, int(LmotorL))  #front left
-#            pwm.set_pwm(8, 0, int(LmotorL))  #back left
-#        else:
-#        #FORWARD BACKWARD MOVEMENT
-#	    LmotorR = Throttle(JoyStick(leftstick)) #(225 * JoyStick(leftstick) + 376)
-#            LmotorL = Throttle(JoyStick((leftstick) * -1)) #(-225 * JoyStick(leftstick) + 376)
-#            pwm.set_pwm(0, 0, int(LmotorR)) #front right
-#            pwm.set_pwm(15, 0, int(LmotorR)) #back right
-#            pwm.set_pwm(4, 0, int(LmotorL))  #front left
-#            pwm.set_pwm(8, 0, int(LmotorL))  #back left
